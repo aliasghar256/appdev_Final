f396a1ca97974f0220f94697fc97a12f1ca07704
unknown <alih11737@gmail.com>
2024-12-27 19:10:43 +0500

    First

---
+++ lib/home_page.dart
@@ -0,0 +1,51 @@
+import 'auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  final Auth auth;
+
+  HomePage({required this.auth});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late User? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentUser = widget.auth.currentUser;
+    widget.auth.authStateChanges.listen((user) {
+      setState(() {
+        _currentUser = user;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome to the Home Page!'),
+            if (_currentUser != null) Text('You are logged in as ${_currentUser!.email}'),
+            ElevatedButton(
+              onPressed: () async {
+                await widget.auth.signOut();
+              },
+              child: Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
