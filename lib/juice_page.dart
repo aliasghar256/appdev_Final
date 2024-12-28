179eb0a65b00fdfae10098531d59f05bcc8bb3a5
Ali Asghar Hussain <alih11737@gmail.com>
2024-12-28 12:32:43 +0500

    Add to cart

--- lib/juice_page.dart
+++ lib/juice_page.dart
@@ -48,54 +48,57 @@ class JuicePage extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Text(
-                            juice['name'],
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Card(
-                              color: Colors.blue,
-                              child: SizedBox(
-                                height: 50,
-                                width: 100,
-                                child: state.quantity == 0
-                                    ? TextButton(
-                                        child: const Text(
-                                          'Add Item',
-                                          style: TextStyle(
-                                            color: Colors.white,
+                          ListTile(
+                            title: Text(
+                              juice['name'],
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            subtitle: Text(state.quantity.toString()),
+                            trailing: Card(
+                                color: Colors.blue,
+                                child: SizedBox(
+                                  height: 50,
+                                  width: 100,
+                                  child: state.quantity == 0
+                                      ? TextButton(
+                                          child: const Text(
+                                            'Add Item',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
                                           ),
+                                          onPressed: () {
+                                            BlocProvider.of<JuiceBloc>(context)
+                                                .add(AddOneToCartEvent());
+                                          },
+                                        )
+                                      : Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            IconButton(
+                                              icon: const Icon(Icons.remove),
+                                              onPressed: () {
+                                                BlocProvider.of<JuiceBloc>(
+                                                        context)
+                                                    .add(
+                                                        RemoveOneFromCartEvent());
+                                              },
+                                            ),
+                                            Text(state.quantity.toString()),
+                                            IconButton(
+                                              icon: const Icon(Icons.add),
+                                              onPressed: () {
+                                                BlocProvider.of<JuiceBloc>(
+                                                        context)
+                                                    .add(AddOneToCartEvent());
+                                              },
+                                            ),
+                                          ],
                                         ),
-                                        onPressed: () {
-                                          BlocProvider.of<JuiceBloc>(context)
-                                              .add(AddOneToCartEvent());
-                                        },
-                                      )
-                                    : Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          IconButton(
-                                            icon: const Icon(Icons.remove),
-                                            onPressed: () {
-                                              BlocProvider.of<JuiceBloc>(
-                                                      context)
-                                                  .add(
-                                                      RemoveOneFromCartEvent());
-                                            },
-                                          ),
-                                          Text(state.quantity.toString()),
-                                          IconButton(
-                                            icon: const Icon(Icons.add),
-                                            onPressed: () {
-                                              BlocProvider.of<JuiceBloc>(
-                                                      context)
-                                                  .add(AddOneToCartEvent());
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                              )),
+                                )),
+                          ),
                         ],
                       ),
                       Text(
